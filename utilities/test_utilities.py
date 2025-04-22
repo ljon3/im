e57940f093b1582efb6c74811df8b8f435dbca5e
Lawrence <ljohny996@gmail.com>
2025-04-22 19:45:04 +0200

    adding pytest file

---
+++ utilities/test_utilities.py
@@ -0,0 +1,18 @@
+import unittest
+from utilities.utils import fullpath, get_datestr, get_datetime, last_working_day, last_day, validate_date
+from datetime import datetime
+import pandas as pd
+
+class TestMyModule(unittest.TestCase):
+
+    def test_datetime(self):
+        self.assertEqual(get_datetime( get_datestr(20231231) ), datetime(2023,12,31))
+
+    def test_datestr(self):
+        self.assertEqual(get_datestr(get_datetime( get_datestr(20231231) )), "20231231")
+
+    def test_last_working_day(self):
+        self.assertEqual(last_working_day(20231231), datetime(2023,12,29))
+
+    def test_last_day(self):
+        self.assertEqual(last_day(20230101), datetime(2023,1,31))
